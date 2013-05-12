70791fdde228a6093c5f94ea1ad72c1d14ea000e
Siarhei Yakushevich <siarheiy@Siarheis-MacBook-Pro.local>
2013-05-12 21:44:38 +0300

    Initial Commit

---
+++ CoolVideoProcessor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoolVideoProcessor
+//
+//  Created by Siarhei Yakushevich on 5/12/13.
+//  Copyright (c) 2013 Siarhei Yakushevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CoolVideoProcessor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoolVideoProcessor
+//
+//  Created by Siarhei Yakushevich on 5/12/13.
+//  Copyright (c) 2013 Siarhei Yakushevich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
